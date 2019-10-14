0b8ecbffbcdbeb448c9b703e1262b05092362930
acidicnut <lorinczb96@gmail.com>
2019-10-14 19:30:01 +0200

    elolvastam a feladatot, de a nagy hazi specre utal... azt is el kell olvasnom

--- khf2.erl
+++ khf2.erl
@@ -3,3 +3,19 @@
 -vsn('year-mm-dd').
 %-export([ertekek/2]).
 -compile(export_all).
+
+-type sspec() :: {size(), board()}.
+-type size()  :: integer().
+-type field() :: [info()].
+-type info()  :: e | o | s | w | integer().
+-type board() :: [[field()]].
+
+-type ssol() :: [[integer()]].
+-type col() :: integer().
+-type row() :: integer().
+-type coords() :: {row(),col()}.
+-spec khf2:ertekek(SSpec :: sspec(), R_C :: coords()) -> Vals :: [integer()].
+%% Egy érték pontosan akkor szerepel a Vals listában, ha teljesíti a
+%% fenti Prolog specifikációban felsorolt (a), (b) és (c) feltételeket, ahol
+%% Vals az SSpec specifikációval megadott Sudoku-feladvány R_C
+%% koordinátájú mezőjében megengedett értékek listája.
