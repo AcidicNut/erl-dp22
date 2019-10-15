333aba5f229a0d113e0a868a387af03e94d26113
acidicnut <lorinczb96@gmail.com>
2019-10-15 16:11:06 +0200

    cellak meg ilyenek

--- khf2.erl
+++ khf2.erl
@@ -15,8 +15,27 @@ elemFilter(L, Elemek) -> elemFilter(torles(hd(Elemek), L), tl(Elemek)).
 sorVizsgalo(L, Row) when (length(Row) < 1) -> L;
 sorVizsgalo(L, Row) -> sorVizsgalo(elemFilter(L, hd(Row)), tl(Row)).
 
-% Vegig iteral Sudoku sorain es minden C. cellaban levo elemet filterezik
+% Vegig iteral Sudoku sorain es minden C. cellaban levo elemet filterezi
 oszlop(L, M, _C) when (length(M) < 1) -> L;
 oszlop(L, M, C) -> oszlop(elemFilter(L, lists:nth(C, hd(M))), tl(M), C).
 
-ertekek({K, M}, {R, C}) -> oszlop(sorVizsgalo(lists:seq(1,K*K), lists:nth(R, M)), M, C).
+% Sorok es oszlopok vizsgalatakor kivesszuk amink volt, ezert most visszaadjuk
+eredetiCellaPlusz(L, M, R, C) -> (L ++ lists:nth(C, lists:nth(R, M))) -- [e,o,w,s].
+
+% R,C koordinata nagy cellajanak kezdo es utolso oszlopa
+kezdoCol(C, K) -> (C div K) * K + 1.
+vegeCol(C, K) -> (C div K) * K + K.
+
+% R,C koordinata nagy cellajanak kezdo es utolso sora
+kezdoRow(R, K) -> (R div K) * K + 1.
+vegeRow(R, K) -> (R div K) * K + K.
+
+% R,C koordinata elemeit itt nem szabad kivenni a lehetosegekbol
+rRow(L, C, K, ActRow) -> sorVizsgalo(sorVizsgalo(L, lists:sublist(ActRow, kezdoCol(C, K), C - kezdoCol(C, K))), lists:sublist(ActRow, C + 1, vegeCol(C,K) - C)).
+
+% Vegig megy R,C koordinata K*K-s cellajan, es filterez
+kszkSorok(L, K, _M, R, _C, ActRow) when (ActRow > (R div K) * K + K) -> L;
+kszkSorok(L, K, M, R, C, ActRow) when (ActRow =:= R) -> kszkSorok(rRow(L, C, K, lists:nth(ActRow, M)), K, M, R, C, ActRow + 1);
+kszkSorok(L, K, M, R, C, ActRow) -> kszkSorok(sorVizsgalo(L, lists:sublist(lists:nth(ActRow, M), kezdoCol(C, K), K)), K, M, R, C, ActRow +1).
+
+ertekek({K, M}, {R, C}) -> kszkSorok(eredetiCellaPlusz(oszlop(sorVizsgalo(lists:seq(1,K*K), lists:nth(R, M)), M, C), M, R, C), K, M, R, C, kezdoRow(R,K)).
